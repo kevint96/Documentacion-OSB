9759852f3fdf184e18346598868b3eeb9c8b3e51
Kevin Torres <ktorres@soaint.com>
2025-03-11 21:03:07 -0500

    Ajuste

--- proyecto_documentacion_servicios.py
+++ proyecto_documentacion_servicios.py
@@ -22,6 +22,7 @@ import inspect
 import ast
 from datetime import datetime
 import difflib
+import glob
 
 def print_with_line_number(msg):
     caller_frame = inspect.currentframe().f_back
@@ -363,15 +364,26 @@ def parse_xsd_file(project_path,xsd_file_path, operation_name, service_url, capa
     ruta_corregida = os.path.abspath(os.path.join(project_path, xsd_file_path))
 
     # DEBUG: Verificar qué hay dentro de `ComponentesComunes`
-    ruta_componentes = os.path.join(project_path, "ComponentesComunes")
-    if os.path.exists(ruta_componentes):
-        st.success(f"Contenido en ComponentesComunes: {os.listdir(ruta_componentes)}")
+    ruta_resources = os.path.join(project_path, "ComponentesComunes", "Resources")
+    if os.path.exists(ruta_resources):
+        st.success(f"Contenido en Resources: {os.listdir(ruta_resources)}")
     else:
-        st.error(f"No se encontró la carpeta: {ruta_componentes}")
+        st.error(f"No se encontró la carpeta: {ruta_resources}")
+        
+    # Ruta esperada
+    ruta_resources = os.path.join(project_path, "ComponentesComunes", "Resources")
+
+    if os.path.exists(ruta_resources):
+        st.success(f"Contenido en Resources: {os.listdir(ruta_resources)}")
+    else:
+        st.error(f"No se encontró la carpeta: {ruta_resources}")
 
     # DEBUG: Mostrar ruta corregida y si el archivo existe
     st.success(f"Nombre del archivo corregido: {ruta_corregida}")
     st.success(f"Existe archivo: {os.path.isfile(ruta_corregida)}")
+    
+    archivos_xsd = glob.glob(os.path.join(project_path, "**/*.XMLSchema"), recursive=True)
+    st.success(f"Archivos encontrados en extraccion_jar: {archivos_xsd}")
 
 
     if xsd_file_path.endswith('.XMLSchema') and os.path.isfile(xsd_file_path):
