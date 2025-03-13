82a8949889f468d4010cde42618af385c3b6206d
Kevin Torres <ktorres@soaint.com>
2025-03-13 11:42:46 -0500

    Ajuste entrega final ajuste bug

--- proyecto_documentacion_servicios.py
+++ proyecto_documentacion_servicios.py
@@ -841,6 +841,7 @@ def extract_osb_services_with_http_provider_id(project_path,operacion_a_document
                                         #st.success(f"operacion_a_documentar: {operacion_a_documentar}")
                                         if not operacion_a_documentar or operation_name == operacion_a_documentar:
                                             #st.success(f"operation_actual: {operation_actual}")
+                                            st.success(f"🔍 Analizando operacion: {operation_actual}")
                                             #st.success(f"service_name: {service_name}")
                                             #st.success(f"operation_name: {operation_name}")
                                             #st.success(f"service_url: {service_url}")
@@ -985,7 +986,7 @@ def generar_documentacion(jar_path, plantilla_path,operacion_a_documentar):
                 
                 if elements['request']:
                     
-                    st.success(f"✅ Proyecto {elem['ruta']}")
+                    st.success(f"✅ Proyecto {elements['ruta']}")
                     st.success(f"🔍 Revisando operacion: {operation}")
                     
                     contiene_cabecera_entrada = False
